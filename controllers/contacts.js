5ade625fd60a8d7880ba9e2f2230728442a0188c
atakan yeşilkayalı <94399983+benevolentsocialplanner@users.noreply.github.com>
2022-12-22 12:58:30 +0300

    Add files via upload

---
+++ controllers/contacts.js
@@ -0,0 +1,96 @@
+const Contact = require('../models/contacts')
+const User = require('../models/users')
+const uuidv1 = require('uuid/v1')
+const contactProperty = []
+exports.addcontact = (req,res) => {
+    
+    const name = req.body.name
+    const number = req.body.number
+    
+    if(!(/^[0-9]+$/.test(number))){
+        return res.json({
+            message:"number must contain only numbers"
+        })
+
+        //400
+    }
+    const contact = new Contact({
+        name: name,
+        number: number
+    })
+    
+    Contact.salt = uuidv1() 
+    contactProperty.push([contact])
+    User.contacts = contactProperty 
+    
+    contact.save()
+        .then((contact) => {
+            console.log(`new contact saved : ${contact}`)
+            //
+        })
+        .catch((err) => {
+            console.log(err.message)
+            //400
+        }) 
+}
+exports.listById = (req,res) =>{
+    const _id = req.body._id
+    Contact.findById(_id, (err,result)=>{
+        if(err){
+            console.log(err.message)
+            //400
+        }
+        if(!result){
+            console.log('no results')
+            //400
+        }
+        
+        console.log(`contact id: ${_id}, result: ${result}`)
+    })
+}
+exports.dashboard = (req,res)=>{
+    console.log("dashboard")
+    res.send({message: "dashboard"})
+}
+exports.deleteById = (req,res) =>{
+    const _id = req.body._id
+    Contact.findByIdAndDelete(_id,(err,result)=>{
+        if(err){
+            console.log(err.message)
+            //400
+        }
+        if(!result){
+            console.log("no contact by that id was found")
+                
+            //404
+        }
+        console.log(` deleteByIdAndDelete result: ${result} `)
+    })
+}
+exports.getAll = (req,res) => {
+    
+    Contact.find({},(err,result)=>{
+        if(err){
+             console.log(err.message)
+        }
+        else{
+            /* res.send({
+                result
+            }) */
+            res.send(User.contacts)
+           // console.log(`all users: ${result}`)
+        }
+    })
+}
+exports.getByName = (req,res) => {
+    const name = req.body.name
+    Contact.find({name}, (err,result)=>{
+        if(err){
+            console.log(err.message)
+        }
+        else{
+            console.log(`fetched by name : ${result}`)
+        }
+    })
+    
+}
